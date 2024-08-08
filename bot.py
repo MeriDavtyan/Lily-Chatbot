17781035881e68cdb2f92e4103843e49174c4580
henriksergoyan <henriksergoyan@gmail.com>
2024-08-08 17:13:13 +0400

    Fixed some bugs

--- bot.py
+++ bot.py
@@ -1,15 +1,48 @@
 import openai
 import streamlit as st
 
-logo_path = '/Users/copa/Desktop/CHATBOT API/8593225.png'
-lily_img = '/Users/copa/Desktop/CHATBOT API/lily_TM_color_RGB.png'
-
+logo_path = "/Users/copa/Desktop/CHATBOT API/8593225.png"
+lily_img = "/Users/copa/Desktop/CHATBOT API/lily_TM_color_RGB.png"
+# Streamlit app layout and styling
+st.set_page_config(
+    page_title="Bot",
+    page_icon=":robot_face:",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
 # Set your OpenAI API key
-openai.api_key = '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'
+openai.api_key = "sk-aqCLkscjEfzjIk0IDqoyWeIWh8APj7b8DVrVVjLipbT3BlbkFJ7x6NfaVAp16kUJFr9FDJdpAj5tcs_45yZwIfVGflMA"
+
+options = [
+    "AI",
+    "Marlon Brando",
+    "Meryl Streep",
+    "Robert De Niro",
+    "Harrison Ford",
+    "Al Pacino",
+    "Tom Hanks",
+    "Audrey Hepburn",
+    "Viola Davis",
+    "Morgan Freeman",
+    "Johnny Depp",
+]
+actor = st.sidebar.selectbox("Select the actor", options=options)
 
 # Initialize session state for storing messages
-if 'messages' not in st.session_state:
-    st.session_state.messages = [{'role': 'system', 'content': 'You are a helpful chat assistant named Lily'}]
+# Initialize session state variables if they are not already set
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+if "actor" not in st.session_state:
+    st.session_state.actor = actor
+if st.session_state.actor != actor:
+    st.session_state.actor = actor
+    st.session_state.messages = [
+        {
+            "role": "system",
+            "content": f"You are {actor}, mimic their talk and answear the way the would do.",
+        }
+    ]
+
 
 # Function to send a message
 def send_message():
@@ -36,47 +69,40 @@ def send_message():
 
     - If `st.session_state.user_input` contains user input, it appends the user's message
       to `st.session_state.messages` with 'role' as 'user' and 'content' as the user's input.
-    
+
     - It sends the accumulated messages to OpenAI's GPT-3.5 model (`model='gpt-3.5-turbo'`)
       for generating a response. The response is appended to `st.session_state.messages` with
       'role' as 'assistant' and 'content' as the generated reply.
-    
+
     - Finally, it clears `st.session_state.user_input` to reset the input field.
 
     """
     if st.session_state.user_input:
-        st.session_state.messages.append({'role': 'user', 'content': st.session_state.user_input})
-        response = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-            messages=st.session_state.messages
+        st.session_state.messages.append(
+            {"role": "user", "content": st.session_state.user_input}
         )
-        reply = response['choices'][0]['message']['content']
-        st.session_state.messages.append({'role': 'assistant', 'content': reply})
-        st.session_state.user_input = ""  # Clear input field
+        response = openai.chat.completions.create(
+            model="gpt-3.5-turbo", messages=st.session_state.messages
+        )
+        reply = response.choices[0].message.content
+        st.session_state.messages.append({"role": "assistant", "content": reply})
 
-# Streamlit app layout and styling
-st.set_page_config(
-    page_title="Lily",
-    page_icon=':cherry_blossom:',
-    layout="centered",  
-    initial_sidebar_state="expanded",
-)
 
 st.markdown(
     """
     <style>
     .stTextInput>div>div>input {
-        background-color: #ffeaf7;
+        background-color: #d2ddfa;
         color: #333;
         font-size: 18px;
     }
     .stButton>button {
-        background-color: #ffb6c1;
+        background-color: #4574f5;
         color: white;
         border-radius: 12px;
     }
     .stButton>button:hover {
-        background-color: #ff91a4;
+        background-color: #1b49c4;
     }
     .message {
         padding: 10px;
@@ -84,11 +110,11 @@ st.markdown(
         margin-bottom: 10px;
     }
     .user {
-        background-color: #ffcce6;
+        background-color: #d2ddfa;
         text-align: right;
     }
     .assistant {
-        background-color: #ccf2ff;
+        background-color:#d2ddfa;
     }
     .logo-img {
         width: 200px;  /* Adjust width as needed */
@@ -99,23 +125,29 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-st.title("🌸 Lily Chatbot")
-a = ("Ask me anything and I'll try my best to help!")
+st.title(f"	💬 {actor} Bot")
+a = "Ask me anything and I'll try my best to help!"
 st.markdown(f"<div class='message assistant'>{a}</div>", unsafe_allow_html=True)
 
 
 # Display messages (excluding system message)
 for message in st.session_state.messages:
-    if message['role'] == 'user':
-        st.markdown(f"<div class='message user'>{message['content']}</div>", unsafe_allow_html=True)
-    elif message['role'] == 'assistant':
-        st.markdown(f"<div class='message assistant'>{message['content']}</div>", unsafe_allow_html=True)
+    if message["role"] == "user":
+        st.markdown(
+            f"<div class='message user'>{message['content']}</div>",
+            unsafe_allow_html=True,
+        )
+    elif message["role"] == "assistant":
+        st.markdown(
+            f"<div class='message assistant'>{message['content']}</div>",
+            unsafe_allow_html=True,
+        )
 
 # User input
 user_input = st.text_input("You: ", key="user_input", on_change=send_message)
 
 # Handle pressing Enter to send message
-if user_input and user_input.endswith('\n'):
+if user_input and user_input.endswith("\n"):
     send_message()
 
 if st.button("Send"):
